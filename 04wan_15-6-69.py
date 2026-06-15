60c949708c749bc73772f620ef79d85d61132ff8
WANHang007 <wanlnw007k@gmail.com>
2026-06-15 09:44:34 +0700

    ฝึกเขียนโปรแกรมเงื่อนไขโดยใช้คำสั่ง if else
    15/06/69

---
+++ 04wan_15-6-69.py
@@ -0,0 +1,7 @@
+print("โปรแกรมเลขคู่หรือเลขคี่กันนะ")
+number = int(input("Enter your number: "))
+if number %2 == 0:
+    print("เลขคู่")
+else:
+    print("เลขคี่")
+print("โดย ธนกร อาจยิ่งยง เลขที่ 4 ม.4/4 \nขอบคุณครับ")
